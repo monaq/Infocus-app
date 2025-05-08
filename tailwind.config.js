a1cc5f82759f5b6d3228ac9b22c924e1b4667949
monaq <spillover427@gmail.com>
2025-05-08 17:07:29 +0900

    style: css플러그인 설정과 폰트 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ['Satoshi', 'system-ui', '-apple-system', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
